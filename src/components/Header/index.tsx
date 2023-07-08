3b4b5e77f18d067977b099db538612fa0aebb103
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-08 14:47:05 +0700

    [update] add nav :sparkles:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,12 @@
-import {
-  HeaderButton,
-  HeaderButtonsContainer,
-  HeaderContainer,
-} from "./styles";
-
-import { MapPin, ShoppingCart } from "phosphor-react";
-
-import LogoImage from "../../assets/logo.svg";
 import { NavLink } from "react-router-dom";
 import { useCart } from "../../hooks/useCart";
+import {
+  HeaderContainer,
+  HeaderButtonsContainer,
+  HeaderButton,
+} from "./styles";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import LogoImage from "../../assets/logo.svg";
 
 export function Header() {
   const { cartQuantity } = useCart();
@@ -21,14 +19,29 @@ export function Header() {
         </NavLink>
 
         <HeaderButtonsContainer>
-          <HeaderButton variant="purple">
+          <NavLink to="/pre-order">
+            <HeaderButton variant="yellow">PRE-ORDER</HeaderButton>
+          </NavLink>
+          <NavLink to="/lounge-menu">
+            <HeaderButton variant="purple">LOUNGE MENU</HeaderButton>
+          </NavLink>
+          <NavLink to="/hamper">
+            <HeaderButton variant="yellow">HAMPER</HeaderButton>
+          </NavLink>
+          <NavLink to="/our-stores">
+            <HeaderButton variant="purple">OUR STORES</HeaderButton>
+          </NavLink>
+          <NavLink to="/about-us">
+            <HeaderButton variant="yellow">ABOUT US</HeaderButton>
+          </NavLink>
+          {/* <HeaderButton variant="purple">
             <MapPin size={20} weight="fill" />
             Ho Chi Minh, VN
-          </HeaderButton>
+          </HeaderButton> */}
           <NavLink to="/completeOrder">
             <HeaderButton variant="yellow">
               {cartQuantity >= 1 && <span>{cartQuantity}</span>}
-              <ShoppingCart size={20} weight="fill" />
+              <ShoppingCart size={30} weight="fill" />
             </HeaderButton>
           </NavLink>
         </HeaderButtonsContainer>
