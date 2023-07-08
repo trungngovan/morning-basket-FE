d8738778046c78548274a00889e0dd12d3fbbded
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-08 14:46:32 +0700

    [update] add tags list :sparkles:

--- src/components/OurProducts/index.tsx
+++ src/components/OurProducts/index.tsx
@@ -1,25 +1,3 @@
-// import { ProductCard } from "../ProductCard";
-// import { TitleText } from "../Typography";
-// import { CoffeeList, OurCoffeesContainer } from "./styles";
-
-// import { products } from "../../mock/coffee";
-
-// export function OurCoffees() {
-//   return (
-//     <OurCoffeesContainer className="container">
-//       <TitleText size="l" color="subtitle">
-//         Product
-//       </TitleText>
-
-//       <CoffeeList>
-//         {products.map((product) => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </CoffeeList>
-//     </OurCoffeesContainer>
-//   );
-// }
-
 import React, { useState } from "react";
 import { ProductCard } from "../ProductCard";
 import { TitleText } from "../Typography";
@@ -27,18 +5,34 @@ import {
   ProductList,
   OurProductsContainer,
   PaginationContainer,
+  TagList,
+  Tag,
 } from "./styles";
 import { Pagination } from "../Pagination";
 
 import { products } from "../../mock/coffee";
 
-export function OurCoffees() {
+export function OurProducts() {
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 12;
-  const totalPages = Math.ceil(products.length / productsPerPage);
+
+  // Create an array of unique tags
+  const tags = [...new Set(products.flatMap((product) => product.tags))];
+
+  // Initialize the selected tag to "all"
+  const [selectedTag, setSelectedTag] = useState("all");
+  const [prevSelectedTag, setPrevSelectedTag] = useState("");
+
+  // Filter the products based on the selected tag
+  const filteredProducts =
+    selectedTag === "all"
+      ? products
+      : products.filter((product) => product.tags.includes(selectedTag));
+
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
+  const currentProducts = filteredProducts.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
@@ -47,12 +41,40 @@ export function OurCoffees() {
     setCurrentPage(pageNumber);
   };
 
+  const handleTagSelect = (tag: string) => {
+    setPrevSelectedTag(selectedTag);
+    setSelectedTag(tag);
+    setCurrentPage(1);
+  };
+
   return (
-    <OurProductsContainer className="container">
+    <OurProductsContainer>
       <TitleText size="l" color="subtitle">
         Products
       </TitleText>
-
+      <TagList>
+        <Tag
+          key={"all"}
+          isActive={selectedTag === "all"}
+          onClick={() => handleTagSelect("all")}
+          variant="yellow"
+        >
+          All
+        </Tag>
+        {tags.map((tag) => (
+          <Tag
+            key={tag}
+            isActive={selectedTag === tag}
+            onClick={() => {
+              setPrevSelectedTag(selectedTag);
+              setSelectedTag(tag);
+            }}
+            variant={selectedTag === tag ? "yellow" : "purple"}
+          >
+            {tag}
+          </Tag>
+        ))}
+      </TagList>
       <ProductList>
         {currentProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
--- src/components/OurProducts/styles.ts
+++ src/components/OurProducts/styles.ts
@@ -1,4 +1,73 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
+
+// export const OurProductsContainer = styled.section`
+//   width: 100%;
+//   margin-top: 2rem;
+
+//   @media (max-width: 700px) {
+//     margin-top: 23rem;
+//   }
+// `;
+
+// export const ProductList = styled.div`
+//   width: 100%;
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   column-gap: 2rem;
+//   row-gap: 3.5rem;
+//   margin-top: 3.5rem;
+
+//   @media (max-width: 1000px) {
+//     grid-template-columns: repeat(3, 1fr);
+//   }
+//   @media (max-width: 800px) {
+//     grid-template-columns: repeat(2, 1fr);
+//   }
+//   @media (max-width: 560px) {
+//     grid-template-columns: 1fr;
+//   }
+// `;
+
+// export const PaginationContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin-top: 2.4rem;
+// `;
+
+// export const TagList = styled.div`
+//   display: flex;
+//   align-items: wrap;
+//   gap: 0.75rem;
+// `;
+
+// interface TagButtonProps {
+//   variant: 'purple' | 'yellow'
+// }
+
+// export const Tag = styled.button<{ isActive: boolean }>`
+//   border: none;
+//   background-color: ${(props) =>
+//     props.isActive ? props.theme.colors['base-button'] : "#f3f3f3"};
+//   color: ${(props) => (props.isActive ? "#fff" : "#333")};
+//   padding: 0.8rem 1.0rem;
+//   border-radius: 2rem;
+//   font-size: 1.2rem;
+//   margin-right: 0.5rem;
+//   margin-bottom: 0.5rem;
+//   cursor: pointer;
+//   transition: all 0.3s ease;
+
+//   &:hover {
+//     background-color: ${(props) =>
+//     props.isActive ? props.theme.colors['base-button'] : "#e0e0e0"};
+//   }
+
+//   &:last-child {
+//     margin-right: 0;
+//   }
+// `;
+
+import styled, { css } from 'styled-components'
 
 export const OurProductsContainer = styled.section`
   width: 100%;
@@ -32,4 +101,65 @@ export const PaginationContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 2.4rem;
+`;
+
+export const TagList = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  white-space: nowrap;
+  margin: 0 1rem; 
+`;
+
+export interface TagButtonProps {
+  isActive?: boolean;
+  variant?: "purple" | "yellow";
+  prevSelectedTag?: string;
+}
+
+export const Tag = styled.button<TagButtonProps>`
+  border: none;
+  background-color: ${(props) => props.isActive ? props.theme.colors['base-button'] : "#f3f3f3"};
+  color: ${(props) => props.isActive ? "#fff" : "#333"};
+  padding: 0.6rem 0.8rem;
+  border-radius: 2rem;
+  font-size: 1.0rem;
+  margin-right: 0.3rem;
+  margin-bottom: 0.3rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: ${(props) => props.isActive ? props.theme.colors['base-button'] : "#e0e0e0"};
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  ${({ variant, theme, isActive }) =>
+    isActive && variant === 'purple' && `
+      background-color: ${theme.colors['brand-purple']};
+      color: ${theme.colors['base-white']};
+    `}
+  
+  ${({ variant, theme, isActive }) =>
+    isActive && variant === 'yellow' && `
+      background-color: ${theme.colors['brand-yellow']};
+      color: ${theme.colors['base-white']};
+    `}
+
+  ${({ variant, theme, isActive, prevSelectedTag }) =>
+    isActive && prevSelectedTag !== "" && prevSelectedTag === "purple" && variant === 'yellow' && `
+      background-color: ${theme.colors['brand-yellow']};
+      color: ${theme.colors['base-white']};
+    `}
+
+  ${({ variant, theme, isActive, prevSelectedTag }) =>
+    isActive && prevSelectedTag !== "" && prevSelectedTag === "yellow" && variant === 'purple' && `
+      background-color: ${theme.colors['brand-purple']};
+      color: ${theme.colors['base-white']};
+    `}
 `;
