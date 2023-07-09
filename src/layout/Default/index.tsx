ff57d81d6213dceae634ce6affc09f5a2bb78c0e
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-09 10:03:18 +0700

    [update] adding footer :lipstick:

--- src/layout/Default/index.tsx
+++ src/layout/Default/index.tsx
@@ -1,13 +1,14 @@
-import { Outlet } from 'react-router-dom'
-import { Header } from '../../components/Header'
-import { LayoutContainer } from './styles'
+import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
-
       <Outlet />
+      <Footer />
     </LayoutContainer>
-  )
+  );
 }
