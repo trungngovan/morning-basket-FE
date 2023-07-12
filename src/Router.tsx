ece8df28b4dc03849db964d9988054d4e1996342
briverse <vumnguyenn@gmail.com>
2023-07-13 00:49:50 +0700

    [fix] comment out About Us

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layout/Default";
 import { Home } from "./pages/Home";
-import { AboutUsPage } from "./pages/AboutUs";
+// import { AboutUsPage } from "./pages/AboutUs";
 import { CompleteOrderPage } from "./pages/CompleteOrder";
 import { OrderConfirmedPage } from "./pages/OrderConfirmed";
 import { SingleProductPage } from "./pages/SingleProduct";
@@ -11,7 +11,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/aboutUs" element={<AboutUsPage />} />
+        {/* <Route path="/aboutUs" element={<AboutUsPage />} /> */}
         <Route path="/completeOrder" element={<CompleteOrderPage />} />
         <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
         <Route path="/product" element={<SingleProductPage />} />{" "}
